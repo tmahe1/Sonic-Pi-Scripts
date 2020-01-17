c5aae80db18078934e03ebf157ebbb61d4c9c23f
tmahe1 <44877528+tmahe1@users.noreply.github.com>
2020-01-17 14:00:41 -0800

    Add files via upload

---
+++ theresbeenstrangerthings.rb
@@ -0,0 +1,20 @@
+use_bpm 160
+live_loop :strange do
+  use_synth :saw
+  play :d2
+  sleep 0.5
+  play :d3
+  sleep 0.5
+  play :d4
+  sleep 0.5
+  play :d5
+  sleep 0.5
+  play :d5
+  sleep 0.5
+  play :d4
+  sleep 0.5
+  play :d3
+  sleep 0.5
+  play :d2
+  sleep 0.5
+end
