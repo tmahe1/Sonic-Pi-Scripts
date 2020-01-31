806b229b9e23ae8284995242587cae38a156e124
tmahe1 <44877528+tmahe1@users.noreply.github.com>
2020-01-31 10:39:16 -0800

    Add files via upload

---
+++ challenge_f_solution.rb
@@ -0,0 +1,44 @@
+=begin
+
+YOUR TASK: Use your knowledge of varialbes and loops to change the
+"rate" of the banger.wav sample. Print to the Log to help you understand 
+what is happening and troubleshoot if you run into difficulty!
+
+=end
+
+# Use this variable to speed up, then slow down the sample
+x = 1
+# Use this variable to store the long file path of your sample
+banger = "C:/Users/yutaka_mahe/Downloads/challenge_f/this_is_a_banger.wav"
+
+
+
+# PLAY THE SAMPLE AT NORMAL SPEED (rate: 1 is the default)
+sample banger
+sleep 3
+
+
+# DECREASE THE SPEED OF THE SAMPLE FOUR TIMES
+x =1
+4.times do
+  print "decrease the rate by 0.1"
+  sample banger, rate: x
+  sleep 3
+  x= x-0.1
+end
+
+
+# INCREASE THE SPEED OF THE SAMPLE 4 TIMES
+x = 1
+4.times do
+  print "increase the rate by 0.1"
+  sample banger, rate: x
+  sleep 3
+  x= x+0.1
+end
+
+
+#PLAY THE SOUND AT A FASTER SPEED (rate > 1)
+with_fx :reverb do
+  sample banger, rate: 3
+end
